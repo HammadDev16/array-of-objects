7ec98d2c9ac5286a46144f2fe9667c3144978c03
M_HAMMAD_16 <hammadpkr2@gmail.com>
2025-01-30 09:29:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+let allQuestion = [
+
+{
+    question : "HTML stand for?",
+    Option : [
+        
+            {answer : "Hypertext Makeup Language", isCorrect : false},
+            {answer : "Hypertext Make Language", isCorrect : false},
+            {answer : "Hypertext Markup Language", isCorrect : true},
+        
+    ]
+},
+
+
+{
+    question : "ul stand for?",
+    Option : [
+        
+            {answer : "unorder lit", isCorrect : false},
+            {answer : "unorder list", isCorrect : true},
+            {answer : "under list", isCorrect : false},
+        
+    ]
+},
+
+
+{
+    question : "UI stand for?",
+    Option : [
+        
+            {answer : "User Interface", isCorrect : true},
+            {answer : "Under Interface", isCorrect : false},
+            {answer : "Umbrella Interface", isCorrect : false},
+        
+    ]
+},
+
+    
+]
